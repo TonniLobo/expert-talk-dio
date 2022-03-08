eb1ba76ff3800511f98100ab53d6e6fdf4339809
TonniLobo <grohli18@hotmail.com>
2022-03-08 16:20:38 -0300

    Projeto criando um banco digital DIO

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+public class Main {
+    public static void main(String[] args) {
+    	/*Cliente Antonni = new Cliente();
+    	Antonni.setNome("Antonni");
+    	
+        Conta cc = new ContaCorrente(Antonni);
+        Conta poupanca = new ContaPoupanca(Antonni);
+        
+        cc.depositar(100);
+        cc.transferir(50, poupanca);
+        
+       cc.imprimirExtrato();
+       poupanca.imprimirExtrato();
+       */
+    }
+}
